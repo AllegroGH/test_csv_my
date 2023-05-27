fab24ab1c00c54b4f9d6e85480009e73968228c8
Allegro <alex.makovka@gmail.com>
2023-05-27 22:34:07 +0300

    add solution

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 import _ from 'lodash';
 
-export default function solution(content){
-  // BEGIN
-  console.log(content);
-  // END
+const parse = (content) => {
+  const result = content
+    .trim()
+    .split('\r\n')
+    .slice(1)
+    .map((el) => el.split(',"'))
+    .map((el) => [el[0], ...el[1].split('",')])
+    .map(([prev, name, next], index) => [...prev.split(','), name, ...next.split(',')]);
+  return result;
+};
+
+const getEmbarkedList = (data) => _.union(data.map((el) => el[11]));
+
+const getGendersRatio = (data) => {
+  const maleCount = data.filter((el) => el[4] === 'male').length;
+  const femaleCount = data.filter((el) => el[4] === 'female').length;
+  return [maleCount, femaleCount];
+};
+
+const getSurvivedPercent = (data) => {
+  const survived = data.filter((el) => Number(el[1]));
+  return `${Math.round((survived.length / data.length) * 100)}%`;
+};
+
+const getANames = (data) =>
+  data
+    .filter((el) => el[3][0] === 'A')
+    .map((el) => el[3])
+    .join('\n');
+
+export default function solution(content) {
+  const data = parse(content);
+  console.log(`Count: ${data.length}`);
+
+  const EmbarkedList = getEmbarkedList(data);
+  console.log(`EmbarkedList: ${EmbarkedList}`);
+
+  const [males, females] = getGendersRatio(data);
+  console.log(`Genders ratio (male/female): ${males}/${females}`);
+
+  const survivedPercent = getSurvivedPercent(data);
+  console.log(`Survived: ${survivedPercent}`);
+
+  const ANames = getANames(data);
+  console.log(`Passenger names beginning with "A": ${ANames}`);
 }
